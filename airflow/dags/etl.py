22c4b50e9776ae85817bf12e255781ddad296677
jmarquez42 <ing.jdj@gmail.com>
2020-10-19 01:35:43 +0000

    First commit

---
+++ airflow/dags/etl.py
@@ -0,0 +1,118 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator, 
+                               LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+
+default_args = {
+    'owner': 'jmarquez42',
+    'start_date': datetime(2019, 9, 15),
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'depends_on_past': False,
+    'schedule_interval': '@hourly'
+    
+}
+
+dag = DAG('etl_sparkify_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='@hourly'
+        )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    region = 'us-west-2',
+    table="public.staging_events",
+    s3_bucket="s3://udacity-dend/log_data",
+    json_path = "s3://udacity-dend/log_json_path.json",
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    region = 'us-west-2',
+    table="public.staging_songs",
+    s3_bucket="s3://udacity-dend/song_data",
+    json_path = "auto",
+)
+
+
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="public.songplays",
+    insertTable = SqlQueries.songplay_table_insert,
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="public.users",
+    insertTable = SqlQueries.user_table_insert,
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="public.songs",
+    insertTable = SqlQueries.song_table_insert,
+    
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="public.artists",
+    insertTable = SqlQueries.artist_table_insert,
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table='public."time"',
+    insertTable = SqlQueries.time_table_insert,
+)
+
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag,
+    field="playid",
+    table='public.songplays',
+    redshift_conn_id="redshift",
+)
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> stage_events_to_redshift
+start_operator >> stage_songs_to_redshift
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+load_user_dimension_table  >> run_quality_checks
+load_song_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+run_quality_checks  >> end_operator
---
+++ airflow/plugins/operators/load_dimension.py
@@ -0,0 +1,28 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadDimensionOperator(BaseOperator):
+
+    ui_color = '#80BD9E'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 table="",
+                 insertTable = "",
+                 *args, **kwargs):
+
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id=redshift_conn_id
+        self.table=table
+        self.insertTable = insertTable
+
+    def execute(self, context):
+        table = self.table
+        insertTable = self.insertTable
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info("Write table {}".format(table))
+        sql = f"INSERT INTO {table} {insertTable}"
+        self.log.info("SQL LoadDimensionOperator \n {}".format(sql))
+        redshift.run(sql)
